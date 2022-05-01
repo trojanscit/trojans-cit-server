c44582a7ba0605ef0b933d29ec4557f2a3de27a8
Lohith <srklohith05@gmail.com>
2022-05-01 19:54:32 +0530

    chnages

--- controllers/sheets.controller.js
+++ controllers/sheets.controller.js
@@ -11,22 +11,26 @@ exports.sheetsPost = async (req, res) => {
 
 	const googleSheets = google.sheets({ version: 'v4', auth: client });
 
-	await googleSheets.spreadsheets.values.append({
-		auth,
-		spreadsheetId: '1LZsclGLfG7tWSvqLhoRHaXBGbRhjdRziSSwokBlA__0',
-		range:
-			event === 'Technical'
-				? 'Technical'
-				: event === 'TrojansCTF'
-				? 'TrojansCTF'
-				: event === 'Non-Technical'
-				? 'Non-Technical'
-				: event === 'Gaming'
-				? 'Gaming'
-				: 'Workshops',
-		valueInputOption: 'USER_ENTERED',
-		resource: {
-			values: [[name, email, phone, department, year, college, event]]
-		}
-	});
+	await googleSheets.spreadsheets.values
+		.append({
+			auth,
+			spreadsheetId: '1LZsclGLfG7tWSvqLhoRHaXBGbRhjdRziSSwokBlA__0',
+			range:
+				event === 'Technical'
+					? 'Technical'
+					: event === 'Trojans CTF'
+					? 'Trojans CTF'
+					: event === 'Non-Technical'
+					? 'Non-Technical'
+					: event === 'Gaming'
+					? 'Gaming'
+					: 'Workshops',
+			valueInputOption: 'USER_ENTERED',
+			resource: {
+				values: [[name, email, phone, department, year, college, event]]
+			}
+		})
+		.then((response) => {
+			res.status(200).json('ok');
+		});
 };
