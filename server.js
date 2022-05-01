55ddbf9eefe6aa64aeef2491e75229a95187d2e4
Prasanna <prasannavelmurugan0200@gmail.com>
2022-05-01 21:56:58 +0530

    razor-pay

--- server.js
+++ server.js
@@ -1,15 +1,85 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const shortid = require("shortid");
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const sheetsRoute = require("./routes/sheets.route");
+const {options} = require("./routes/sheets.route");
 
 const app = express();
 
-const sheetsRoute = require('./routes/sheets.route');
+app.use(cors());
 
 app.use(express.json());
+app.use(bodyParser.json());
 
-app.use('/api', sheetsRoute);
+app.use("/api", sheetsRoute);
+
+app.get("/logo", (req, res) => {
+    res.sendFile(path.join(__dirname, "logo.png"));
+});
+
+const razorpay = new Razorpay({
+    key_id: "rzp_test_mAa9xKRpHr07z0",
+    key_secret: "JihrpKhfeDLqVAtx6iVcOQVz",
+});
+
+app.post("/razorpay", async (req, res) => {
+    const payment_capture = 1;
+    const amount = 499;
+    const currency = "INR";
+
+    const options = {
+        amount: amount * 100,
+        currency,
+        receipt: shortid.generate(),
+        payment_capture,
+    };
+
+    try {
+        const response = await razorpay.orders.create(options);
+        console.log(response);
+        res.json({
+            id: response.id,
+            currency: response.currency,
+            amount: response.amount,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post("/verification", async (req, res) => {
+    const secret = "";
+
+    console.log(req.body);
+
+    const crypto = require("crypto");
+
+    const shasum = crypto.createHmac("sha256", secret);
+    shasum.update(JSON.stringify(req.body));
+    const digest = shasum.digest("hex");
+
+    console.log(digest, req.headers["x-razorpay-signature"]);
+
+    if (digest === req.headers["x-razorpay-signature"]) {
+        console.log("verified");
+
+        // require("fs").writeFileSync(
+        //     "./verification.json",
+        //     JSON.stringify(req.body, null, 4)
+        // );
+    } else {
+        console.log("not verified");
+    }
+
+    res.json({status: "ok"});
+});
 
 app.listen(process.env.PORT, () =>
-	console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`)
+    console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`)
 );
