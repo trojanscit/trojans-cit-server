81551d208b881b0f076466bd95fa766e55c8d942
Lohith <srklohith05@gmail.com>
2022-05-08 09:53:07 +0530

    changes

--- controllers/sheets.controller.js
+++ controllers/sheets.controller.js
@@ -9,9 +9,13 @@ exports.sheetsPost = async (req, res) => {
 		year,
 		college,
 		event,
-		workshop,
-		gaming
+		workshops,
+		gaming,
+		paper,
+		abstract,
+		presentation
 	} = req.body;
+
 	const auth = new google.auth.GoogleAuth({
 		keyFile: './credentials.json',
 		scopes: 'https://www.googleapis.com/auth/spreadsheets'
@@ -21,40 +25,71 @@ exports.sheetsPost = async (req, res) => {
 
 	const googleSheets = google.sheets({ version: 'v4', auth: client });
 
-	await googleSheets.spreadsheets.values
-		.append({
-			auth,
-			spreadsheetId: '1LZsclGLfG7tWSvqLhoRHaXBGbRhjdRziSSwokBlA__0',
-			range:
-				event === 'Technical and Non-Technical'
-					? 'Technical and Non-Technical'
-					: event === 'Trojans CTF'
-					? 'Trojans CTF'
-					: event === 'Gaming'
-					? 'Gaming'
-					: 'Workshops',
-			valueInputOption: 'USER_ENTERED',
-			resource: {
-				values: [
-					[
-						name,
-						email,
-						phone,
-						department,
-						year,
-						college,
-						event === 'Workshops'
-							? workshop
-							: event === 'Gaming'
-							? gaming
-							: event,
-						false,
-						new Date().toDateString()
+	if (event != 'Paper Bytes') {
+		await googleSheets.spreadsheets.values
+			.append({
+				auth,
+				spreadsheetId: '1LZsclGLfG7tWSvqLhoRHaXBGbRhjdRziSSwokBlA__0',
+				range:
+					event === 'Technical and Non-Technical'
+						? 'Technical and Non-Technical'
+						: event === 'Trojans CTF'
+						? 'Trojans CTF'
+						: event === 'Gaming'
+						? 'Gaming'
+						: 'Workshops',
+				valueInputOption: 'USER_ENTERED',
+				resource: {
+					values: [
+						[
+							name,
+							email,
+							phone,
+							department,
+							year,
+							college,
+							event === 'Workshops'
+								? workshops
+								: event === 'Gaming'
+								? gaming
+								: event,
+							false,
+							new Date().toDateString()
+						]
 					]
-				]
-			}
-		})
-		.then((response) => {
-			res.status(200).json('ok');
-		});
+				}
+			})
+			.then((response) => {
+				res.status(200).json('ok');
+			});
+	} else {
+		await googleSheets.spreadsheets.values
+			.append({
+				auth,
+				spreadsheetId: '1LZsclGLfG7tWSvqLhoRHaXBGbRhjdRziSSwokBlA__0',
+				range: 'Paper Bytes',
+				valueInputOption: 'USER_ENTERED',
+				resource: {
+					values: [
+						[
+							name,
+							email,
+							phone,
+							department,
+							year,
+							college,
+							event,
+							paper,
+							abstract,
+							presentation,
+							false,
+							new Date().toDateString()
+						]
+					]
+				}
+			})
+			.then((response) => {
+				res.status(200).json('ok');
+			});
+	}
 };
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const sheetsRoute = require('./routes/sheets.route');
+
 // const razorpayRoute = require('./routes/razorpay.route');
 const contactRoute = require('./routes/contact.route');
 const AuthRoute = require('./routes/auth.route');
